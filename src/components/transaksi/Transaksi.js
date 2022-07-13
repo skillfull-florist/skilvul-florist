c60df26a6fdddc2cf47183fca505946976c0b4c4
Miananur <miananurhlz@gmail.com>
2022-07-13 12:18:18 +0700

    transaksi coba

--- src/components/transaksi/Transaksi.js
+++ src/components/transaksi/Transaksi.js
@@ -64,106 +64,106 @@ function Transaksi() {
     <div>
     <Row>
       <Col >
-    <Alert style={{backgroundColor:'#E4DCCF', color:'black', marginTop:'5px', marginLeft:'10px', border: "1px solid #A48868"}}>
-      <Alert.Heading style={{textAlign:'left'}}>Pilih Metode Pembayaran {params.id}</Alert.Heading> <hr />
+      <Alert style={{backgroundColor:'#E4DCCF', color:'black', marginTop:'5px', marginLeft:'10px', border: "1px solid #A48868"}}>
+        <Alert.Heading style={{textAlign:'left'}}>Pilih Metode Pembayaran {params.id}</Alert.Heading> <hr />
 
-      <Alert style={myStyle}>
-      <Row>
-          <Col style={{textAlign:'left'}}>
-          <img src={produk.gambar} style={{width:"50%"}}/>
-          <h5>{produk.nama}</h5>
-          </Col>
-        </Row>
-      </Alert>
+          <Alert style={myStyle}>
+          <Row>
+              <Col style={{textAlign:'left'}}>
+              <img src={produk.gambar} style={{width:"50%"}}/>
+              <h5>{produk.nama}</h5>
+              </Col>
+            </Row>
+          </Alert>
 
-      <Alert style={myStyle}>
-      <p style={{textAlign:'left'}}><input type='radio' name='jenis_kelamin' value='pria' /> Florist-pay <space/>
-      <img src={poto2} style={{width:'50px'}}/></p>
-      <hr />
-      <p style={{textAlign:'left'}}><input type='radio' name='jenis_kelamin' value='perempuan' /> Transfer Bank <space/>
-      <img src={poto1} style={{width:'80px',  marginLeft:'20px'}}/><space/>
-      <img src={poto3} style={{width:'100px', marginLeft:'10px'}}/><space/>
-      <img src={poto4} style={{width:'100px'}}/>
-      </p>
-      <hr />
-      <p style={{textAlign:'left'}}><input type='radio' name='jenis_kelamin' value='perempuan' /> Transfer Virtual Akun<space/>
-      <img src={poto1} style={{width:'80px',  marginLeft:'20px'}}/><space/>
-      <img src={poto3} style={{width:'100px',marginLeft:'10px'}}/><space/>
-      <img src={poto4} style={{width:'100px'}}/></p>
-    </Alert>
+          <Alert style={myStyle}>
+          <p style={{textAlign:'left'}}><input type='radio' name='jenis_kelamin' value='pria' /> Florist-pay <space/>
+          <img src={poto2} style={{width:'50px'}}/></p>
+          <hr />
+          <p style={{textAlign:'left'}}><input type='radio' name='jenis_kelamin' value='perempuan' /> Transfer Bank <space/>
+          <img src={poto1} style={{width:'80px',  marginLeft:'20px'}}/><space/>
+          <img src={poto3} style={{width:'100px', marginLeft:'10px'}}/><space/>
+          <img src={poto4} style={{width:'100px'}}/>
+          </p>
+          <hr />
+          <p style={{textAlign:'left'}}><input type='radio' name='jenis_kelamin' value='perempuan' /> Transfer Virtual Akun<space/>
+          <img src={poto1} style={{width:'80px',  marginLeft:'20px'}}/><space/>
+          <img src={poto3} style={{width:'100px',marginLeft:'10px'}}/><space/>
+          <img src={poto4} style={{width:'100px'}}/></p>
+        </Alert>
     
-    <hr />
-      <Alert style={{backgroundColor:"#F5EAD1", border: "1px solid #A48868"}}>
-          Mohon segera lakukan pembayaran
-      </Alert>
-
-    </Alert>
-    </Col>
-
-    <Col>
-    <Alert style={{backgroundColor:'#E4DCCF', color:'black',marginTop:'5px', marginRight:'10px',border: "1px solid #A48868"}}> 
-      <Alert.Heading style={{textAlign:'left'}}>Ringkasan Pembelian</Alert.Heading><hr />
-      <p style={{textAlign:'left'}}><a href="https://react-bootstrap.github.io/components/overlays/#tooltips">
-        Punya kode voucher?
-      </a></p>
       <hr />
-      <Alert style={myStyle}>
-      <p style={{textAlign:'left'}}><input type='checkbox' name='jenis_kelamin' value='perempuan' /> Bayar sebagian dengan saldo.Saldo anda : Rp 90.000</p></Alert>
-      <hr/>
-      <Alert style={myStyle}>
-        <Row>
-          <Col style={{textAlign:'left'}}>
-          <p>Harga Barang : </p>
-          </Col>
-          <Col style={{textAlign:'Right'}}>
-          Rp {produk.harga}
-          </Col>
-        </Row>
-        <Row>
-          <Col style={{textAlign:'left'}}>
-          <p>Biaya Kirim & Penanganan :</p>
-          </Col>
-          <Col style={{textAlign:'Right'}}>
-          Rp 40.000
-          </Col>
-        </Row>
-        <Row>
-          <Col style={{textAlign:'left'}}>
-          <p>Biaya Perlindungan Barang :</p>
-          </Col>
-          <Col style={{textAlign:'Right'}}>
-          Rp 500
-          </Col>
-        </Row>
-        <Row>
-          <Col style={{textAlign:'left'}}>
-          <p>Biaya Pelayanan :</p>
-          </Col>
-          <Col style={{textAlign:'Right'}}>
-          Rp 2.500
-          </Col>
-        </Row>
+        <Alert style={{backgroundColor:"#F5EAD1", border: "1px solid #A48868"}}>
+            Mohon segera lakukan pembayaran
+        </Alert>
+
       </Alert>
-      <hr/>
-      <Row>
-          <Col style={{textAlign:'left'}}>
-          <p>Total Harga :</p>
-          </Col>
-          <Col style={{textAlign:'Right', color:'red'}}>
-          <b>Rp {produk.harga*2}</b>
-          </Col>
-        </Row>
-      <div className="d-grid gap-2">
-      <Button onClick={() => setModalShow(true)} variant="outline-danger" size="lg" style={{height:'50px'}}>
-        <p style={{fontSize:'15px', fontFamily:"sans-serif"}}>Bayar</p>
-      </Button>
-      <MyVerticallyCenteredModal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
-      </div>
-    </Alert>
-    </Col>
+      </Col>
+
+      <Col>
+      <Alert style={{backgroundColor:'#E4DCCF', color:'black',marginTop:'5px', marginRight:'10px',border: "1px solid #A48868"}}> 
+        <Alert.Heading style={{textAlign:'left'}}>Ringkasan Pembelian</Alert.Heading><hr />
+        <p style={{textAlign:'left'}}><a href="https://react-bootstrap.github.io/components/overlays/#tooltips">
+          Punya kode voucher?
+        </a></p>
+        <hr />
+        <Alert style={myStyle}>
+        <p style={{textAlign:'left'}}><input type='checkbox' name='jenis_kelamin' value='perempuan' /> Bayar sebagian dengan saldo.Saldo anda : Rp 90.000</p></Alert>
+        <hr/>
+        <Alert style={myStyle}>
+          <Row>
+            <Col style={{textAlign:'left'}}>
+            <p>Harga Barang : </p>
+            </Col>
+            <Col style={{textAlign:'Right'}}>
+            Rp {produk.harga}
+            </Col>
+          </Row>
+          <Row>
+            <Col style={{textAlign:'left'}}>
+            <p>Biaya Kirim & Penanganan :</p>
+            </Col>
+            <Col style={{textAlign:'Right'}}>
+            Rp 40.000
+            </Col>
+          </Row>
+          <Row>
+            <Col style={{textAlign:'left'}}>
+            <p>Biaya Perlindungan Barang :</p>
+            </Col>
+            <Col style={{textAlign:'Right'}}>
+            Rp 500
+            </Col>
+          </Row>
+          <Row>
+            <Col style={{textAlign:'left'}}>
+            <p>Biaya Pelayanan :</p>
+            </Col>
+            <Col style={{textAlign:'Right'}}>
+            Rp 2.500
+            </Col>
+          </Row>
+        </Alert>
+        <hr/>
+        <Row>
+            <Col style={{textAlign:'left'}}>
+            <p>Total Harga :</p>
+            </Col>
+            <Col style={{textAlign:'Right', color:'red'}}>
+            <b>Rp {produk.harga*2}</b>
+            </Col>
+          </Row>
+        <div className="d-grid gap-2">
+        <Button onClick={() => setModalShow(true)} variant="outline-danger" size="lg" style={{height:'50px'}}>
+          <p style={{fontSize:'15px', fontFamily:"sans-serif"}}>Bayar</p>
+        </Button>
+        <MyVerticallyCenteredModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
+        </div>
+      </Alert>
+      </Col>
     </Row>
     </div>
   );
